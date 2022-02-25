f54a15b6b62fe8ed7106fde58fda1fb32aeac914
Hemendra Khatik <hemendra.khatik010698@gmail.com>
2022-02-25 19:25:14 +0530

    new sections added

--- src/components/Home/ThirdSection/index.js
+++ src/components/Home/ThirdSection/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 import Style from "./index.module.css";
+import CompanyOne from "../../../assets/home/company1.png";
+import CompanyTwo from "../../../assets/home/company2.png";
+import CompanyThree from "../../../assets/home/company3.png";
+// import Mike from "../../../assets/home/mike.svg";
 
 export default function ThirdSection() {
   return (
@@ -7,6 +11,13 @@ export default function ThirdSection() {
       <h1 className={Style.mainHeading}>
         Diese Unternehmen vertrauen jesam consulting
       </h1>
+      <div className={Style.companies}>
+        <img src={CompanyThree} alt="icon " />
+        <img src={CompanyTwo} alt="icon " />
+        <img src={CompanyOne} alt="icon " />
+      </div>
+
+      {/* <img src={Mike} alt="icon " /> */}
     </div>
   );
 }
