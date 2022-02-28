2c1f28a0065702c440733db193f3f30db22477ad
Hemendra Khatik <hemendra.khatik010698@gmail.com>
2022-02-28 19:44:13 +0530

    removed unused code

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import FirstSection from "../components/Home/FirstSection";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
 import HeroSection from "../components/Home/HeroSection";
 import SecondSection from "../components/Home/SecondSection";
 import ThirdSection from "../components/Home/ThirdSection";
