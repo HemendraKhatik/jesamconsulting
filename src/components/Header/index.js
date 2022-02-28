9c42cbebe536d94f63f4dd289f752bd62198c0fe
Hemendra Khatik <hemendra.khatik010698@gmail.com>
2022-02-28 18:16:34 +0530

    close icon or close navbar added

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Style from "./index.module.css";
 import Logo from "../../assets/logo/logo.svg";
 import Hamburger from "../../assets/icons/hamburger.svg";
+import CloseIcon from "../../assets/icons/closeIcon.svg";
 
 export default function Header() {
+  const [isNavBarVisible, setIsNavBarVisible] = useState(false);
+
   return (
     <header className={Style.headerContainer}>
       <img src={Logo} alt="jesamconsulting logo" />
-      <nav>
+      <nav className={Style.nav}>
         <ul>
           <li>WEBSITE</li>
           <li>APPLIKATION</li>
@@ -15,7 +18,24 @@ export default function Header() {
           <li>KONTAKT</li>
         </ul>
       </nav>
-      <img className={Style.hamburger} src={Hamburger} alt="jesamconsulting hamburger" />
+
+      {isNavBarVisible && (
+        <nav className={Style.mobileNav}>
+          <ul>
+            <li className={Style.activemobilelink}>WEBSITE</li>
+            <li>APPLIKATION</li>
+            <li>SOFTWARE</li>
+            <li>KONTAKT</li>
+          </ul>
+        </nav>
+      )}
+
+      <img
+        onClick={() => setIsNavBarVisible(!isNavBarVisible)}
+        className={Style.hamburger}
+        src={isNavBarVisible ? CloseIcon : Hamburger}
+        alt="jesamconsulting hamburger"
+      />
     </header>
   );
 }
