9841fe8c5341d585b83abe757bb371b8afabf5d1
Hemendra Khatik <hemendra.khatik010698@gmail.com>
2022-02-28 22:05:44 +0530

    carousel added

---
+++ src/components/Website/HeroSection/data.js
@@ -0,0 +1,17 @@
+export const carouselData = [
+  {
+    title: "Entwicklung benutzerdefinierter Anwendungen",
+    description:
+      "Wir verlassen uns auf unsere branchenspezifische Technologieerfahrung, um hochskalierbare, flexible und interoperable Web-, Mobil-, Desktop- und Hybridanwendungen bereitzustellen.",
+  },
+  {
+    title: "Anwendungswartung",
+    description:
+      "Unsere App-Wartungs- und Modernisierungsservices sind darauf ausgelegt, die Skalierbarkeit, Leistung und Nachhaltigkeit Ihrer gesamten Softwareinfrastruktur sicherzustellen.",
+  },
+  {
+    title: "Benutzerdefinierte Webentwicklung",
+    description:
+      "Unsere kundenspezifischen Lösungen beschleunigen Arbeitsabläufe, steigern den Umsatz und optimieren den Geschäftsbetrieb vom Konzept bis zum Code und von der Entwicklung bis zur Bereitstellung.",
+  },
+];
--- src/components/Website/HeroSection/index.js
+++ src/components/Website/HeroSection/index.js
@@ -1,28 +1,33 @@
 import React from "react";
 import Style from "./index.module.css";
 import HeroImg from "../../../assets/website/hero.png";
-import Slider from "../../Slider";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "./carousel.css";
+import { carouselData } from "./data";
 
 export default function HeroSection() {
   return (
-    <div className={Style.container}>
-      <div className={Style.left}>
-        <h1 className={Style.mainHeading}>
-          Entwicklung benutzerdefinierter Anwendungen
-        </h1>
-
-        <p className={Style.mainParagraph}>
-          Wir verlassen uns auf unsere branchenspezifische Technologieerfahrung,
-          um hochskalierbare, flexible und interoperable Web-, Mobil-, Desktop-
-          und Hybridanwendungen bereitzustellen.
-        </p>
-        <div className={Style.slider}>
-          <Slider />
-        </div>
-      </div>
-      <div className={Style.right}>
-        <img src={HeroImg} alt="jesamconsulting" />
-      </div>
-    </div>
+    <Carousel
+      showThumbs={false}
+      showStatus={false}
+      showArrows={false}
+      autoPlay
+      verticalSwipe="natural"
+    >
+      {carouselData.map((slide, index) => {
+        return (
+          <div key={index} className={Style.container}>
+            <div className={Style.left}>
+              <h1 className={Style.mainHeading}>{slide.title}</h1>
+              <p className={Style.mainParagraph}>{slide.description}</p>
+            </div>
+            <div className={Style.right}>
+              <img src={HeroImg} alt="jesamconsulting" />
+            </div>
+          </div>
+        );
+      })}
+    </Carousel>
   );
 }
