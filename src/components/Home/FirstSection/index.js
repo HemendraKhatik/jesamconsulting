01d22e3d450a412685653ebf1f023614cc9aeab1
Hemendra Khatik <hemendra.khatik010698@gmail.com>
2022-02-28 13:28:31 +0530

    tablet responsive css added

--- src/components/Home/FirstSection/index.js
+++ src/components/Home/FirstSection/index.js
@@ -17,18 +17,17 @@ export default function FirstSection() {
     <div className={Style.firstSectionContainer}>
       <div className={Style.left}>
         <h1 className={Style.mainHeading}>
-          mehr <br />
-          Performance
-          <br /> bedeutet nicht <br />
-          weniger design
+          mehr Performance bedeutet nicht weniger design
         </h1>
         <p className={Style.mainParagraph}>
           Für uns steht die Performance an erster Stelle, jede von JESAM
           Consulting entwickelte Software erfüllt höchste
-          Qualitätsstandards.JESAM ist immer up-to-date und erfüllt höchste
+          Qualitätsstandards.<br/>
+          JESAM ist immer up-to-date und bietet höchste
           Sicherheits und Entwicklungsstandards.
         </p>
       </div>
+
       <div className={Style.right}>
         <img
           className={animate ? Style.bubble : null}
