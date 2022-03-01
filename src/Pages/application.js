735d6640f7f71e4b59be3f61a08b0d53570d4881
Hemendra Khatik <hemendra.khatik010698@gmail.com>
2022-03-01 16:51:24 +0530

    removed unused code

---
+++ src/Pages/application.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Base from "./Base";
+
+export default function Application() {
+  return (
+    <React.Fragment>
+      <Base>
+        <h1>Hello from application</h1>
+      </Base>
+    </React.Fragment>
+  );
+}
--- src/Pages/website.js
+++ src/Pages/website.js
@@ -1,5 +1,4 @@
 import React from "react";
-import WhyWe from "../components/Reusable/WhyWe";
 import FirstSection from "../components/Website/FirstSection";
 import HeroSection from "../components/Website/HeroSection";
 import Base from "./Base";
